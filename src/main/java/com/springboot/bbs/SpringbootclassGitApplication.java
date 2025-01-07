4a0d0ae1ae259f1141f2ff59053e327dbc03e6ad
Mincheol-Ha <hmc0501@gmail.com>
2025-01-07 09:56:07 +0900

    initial commit

---
+++ src/main/java/com/springboot/bbs/SpringbootclassGitApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootclassGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootclassGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/bbs/SpringbootclassGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootclassGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
